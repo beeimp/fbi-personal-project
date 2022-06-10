25cc8b546ec54f1ac77d924ed6c99c4982841386
BeeImp <zero9231@naver.com>
2022-06-10 17:54:42 +0900

    Feat: 지갑 연결 확인

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,33 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  // 카이카스 지갑 잠금 확인
+  const checkKaiKas = () => {
+    if (window?.klaytn?._kaikas) {
+      window.klaytn._kaikas.isUnlocked().then((isUnlocked: boolean) => {
+        if (!isUnlocked) {
+          router.push('/auth');
+        }
+      });
+    } else {
+      router.push('/auth');
+    }
+  }
+
+  useEffect(() => {
+    checkKaiKas();
+
+    document.addEventListener('mousedown', checkKaiKas);
+    return () => {
+      document.removeEventListener('mousedown', checkKaiKas);
+    }
+  })
   return <Component {...pageProps} />
 }
 
