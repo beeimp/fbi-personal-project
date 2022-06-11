4566e5ce19d948e827fc6baa117967bd6f0a6bb7
BeeImp <zero9231@naver.com>
2022-06-12 00:42:35 +0900

    Fix: 오타 및 css -> scss

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,13 +1,12 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
-
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
-  // 카이카스 지갑 잠금 확인
+  // 카이카스 지갑 연결 확인
   const checkKaiKas = () => {
     if (window?.klaytn?._kaikas) {
       window.klaytn._kaikas.isUnlocked().then((isUnlocked: boolean) => {
