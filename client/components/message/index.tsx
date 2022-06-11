98b2185a779fe6be632ba3a4d2b1a734ceac4eb8
BeeImp <zero9231@naver.com>
2022-06-12 00:51:23 +0900

    Add: message 컴포넌트 export

---
+++ client/components/message/index.tsx
@@ -0,0 +1,11 @@
+import MessageBalloon from "./balloon";
+import MessageDateTime from './datetime';
+import MessageInput from './input';
+import MessageListItem from './list-item';
+
+export {
+  MessageBalloon,
+  MessageDateTime,
+  MessageInput,
+  MessageListItem
+}
