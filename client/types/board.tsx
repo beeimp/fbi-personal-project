ba3934453ecc61846c673c78dd584723c5f06081
BeeImp <zero9231@naver.com>
2022-06-17 15:41:52 +0900

    Add: commentType, registerInfoType

--- client/types/board.tsx
+++ client/types/board.tsx
@@ -1,12 +1,25 @@
+import { userInfoType } from "./user";
+
 type contentsType = {
-    id: number,
-    image_urls: Array<string>,
-    article: string,
-    create_date: number
-    hashtag: Array<string>,
-    
+  id: number;
+  image_urls: Array<string>;
+  article: string;
+  create_date: number;
+  hashtag: Array<string>;
+  up_voting: number;
+  down_voting: number;
 };
 
+type commentType = {
+  id: string;
+  user_info: userInfoType;
+  comment: string;
+  create_date: number;
+  up_voting: number;
+  down_voting: number;
+}
+
 export type {
-  contentsType
+  contentsType,
+  commentType
 };
---
+++ client/types/user.tsx
@@ -0,0 +1,18 @@
+type registerInfoType = {
+  address: string;
+  useranme: string;
+  phoneNumber: string;
+  thumbnail: string;
+}
+
+type userInfoType = {
+  address: string;
+  username: string;
+  thumbnail: string;
+}
+
+
+export type {
+  registerInfoType,
+  userInfoType
+}
