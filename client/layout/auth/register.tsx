119117737d3e50f30bcac71c7e67906c52cd7539
BeeImp <zero9231@naver.com>
2022-06-17 15:46:34 +0900

    Refactor: 회원가입 타입명 수정

--- client/layout/auth/register.tsx
+++ client/layout/auth/register.tsx
@@ -1,38 +1,38 @@
 import { ChangeEventHandler, Dispatch, FunctionComponent, MouseEventHandler, SetStateAction, useEffect } from "react";
 import { AuthWrapper, AuthTitle, AuthInput, AuthAddress, AuthButton } from '../../components/auth';
 import { ProfileAvatar } from '../../components/profile';
-import { userInfoType } from "../../types/auth";
+import { registerInfoType } from "../../types/user";
 import { useRouter } from 'next/router';
 interface AuthRegisterProps {
   title: string;
-  userInfo: userInfoType;
-  setUserInfo: Dispatch<SetStateAction<userInfoType>>;
+  registerInfo: registerInfoType;
+  setRegisterInfo: Dispatch<SetStateAction<registerInfoType>>;
 }
 
-const AuthRegister: FunctionComponent<AuthRegisterProps> = ({ title, userInfo, setUserInfo }) => {
+const AuthRegister: FunctionComponent<AuthRegisterProps> = ({ title, registerInfo, setRegisterInfo }) => {
   const router = useRouter();
 
   useEffect(() => {
     if (window && window.klaytn) {
-      if (userInfo.address === "") {
-        setUserInfo({
-          ...userInfo,
+      if (registerInfo.address === "") {
+        setRegisterInfo({
+          ...registerInfo,
           address: window.klaytn.selectedAddress
         })
       }
       window.klaytn.on('accountsChanged', function (account: string[]) {
         // kaikas에서 계정을 변경할 때 마다 내부의 함수가 실행됩니다.
-        setUserInfo({
-          ...userInfo,
+        setRegisterInfo({
+          ...registerInfo,
           address: account[0]
         })
       });
     }
-  }, [setUserInfo, userInfo])
+  }, [setRegisterInfo, registerInfo])
 
   const inputChangeHandler: ChangeEventHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUserInfo({
-      ...userInfo,
+    setRegisterInfo({
+      ...registerInfo,
       [event.target.name]: event.target.value
     })
   }
@@ -46,7 +46,7 @@ const AuthRegister: FunctionComponent<AuthRegisterProps> = ({ title, userInfo, s
     <AuthWrapper>
       <AuthTitle title={title} mode="register"></AuthTitle>
       <ProfileAvatar size="200px"></ProfileAvatar>
-      <AuthAddress address={userInfo.address}></AuthAddress>
+      <AuthAddress address={registerInfo.address}></AuthAddress>
       <AuthInput label="닉네임" name="username" onChange={inputChangeHandler}></AuthInput>
       <AuthInput label="전화번호" name="phoneNumber" onChange={inputChangeHandler}></AuthInput>
       <AuthButton onClick={submitHandler}>회원가입</AuthButton>
